3721780a4e5a81c72f18adb4308d5deab71360fc
Josh Gearheart <other.josh.gearheart@gmail.com>
2021-04-30 14:36:31 -0700

    add basic form input UI

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 app.listen(3000, function () {
   console.log("listening on 3000");
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+});
