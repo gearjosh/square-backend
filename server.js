23301f71e0c5a8c63294cd46cdf90430db081ae4
Josh Gearheart <other.josh.gearheart@gmail.com>
2021-04-30 15:27:54 -0700

    refactor slightly

--- server.js
+++ server.js
@@ -4,27 +4,26 @@ const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/squares", (req, res) => {
-  console.log(req.body);
-});
-
 MongoClient.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.eu7jt.mongodb.net/squares?retryWrites=true&w=majority`,
-  {
-    useUnifiedTopology: true,
-  },
-  (err, client) => {
-    if (err) return console.error(err);
-    console.log("Connected to Database");
-  }
-);
+  { useUnifiedTopology: true }
+).then((client) => {
+  console.log("Connected to Database");
+
+  const db = client.db("squares");
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+  
+  app.post("/squares", (req, res) => {
+    console.log(req.body);
+  });
+  
+  app.listen(3000, function () {
+    console.log("listening on 3000");
+  });
+
+}).catch(console.error);
